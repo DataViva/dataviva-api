28ab2a49092792453f448ab6b29f5faef143f4cc
Laura de Bella <lauradebella@gmail.com>
2016-12-22 17:59:05 -0200

    Update column names of secex model

--- app/models/secex.py
+++ app/models/secex.py
@@ -5,21 +5,23 @@ class Secex(db.Model):
     __tablename__ = 'secex'
     year = Column(Integer, primary_key=True)
     month = Column(Integer, primary_key=True)
-    hs_07 = Column(String(4), primary_key=True)
+    product = Column(String(4), primary_key=True)
     country = Column(String(3), primary_key=True)
-    uf_ibge = Column(String(2), primary_key=True)
-    sh4 = Column(String(4), primary_key=True)
-    mun_ibge = Column(String(7), primary_key=True)
+    state = Column(String(2), primary_key=True)
+    port = Column(String(4), primary_key=True)
+    municipality = Column(String(7), primary_key=True)
     value = Column(Numeric(17,2))
     kg = Column(Numeric(17,2))
+    trade_type = Column(String(6), primary_key=True)
 
     def __iter__(self):
         yield "year", self.year
         yield "month", self.month
-        yield "hs_07", self.hs_07
+        yield "product", self.product
         yield "country", self.country
-        yield "uf_ibge", self.uf_ibge
-        yield "sh4", self.sh4
-        yield "mun_ibge", self.mun_ibge
+        yield "state", self.state
+        yield "port", self.port
+        yield "municipality", self.municipality
         yield "value", str(self.value)
         yield "kg", str(self.kg)
+        yield "trade_type", self.trade_type
