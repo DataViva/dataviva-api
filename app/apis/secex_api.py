d224205ada02f34a920621c769bc4d89a3c68ed3
Diogo Lundberg <diogo@lundberg.com.br>
2016-12-23 11:17:16 -0200

    Refactors secex/states so it can get dimensions by the path

--- app/apis/secex_api.py
+++ app/apis/secex_api.py
@@ -1,19 +1,26 @@
 from flask import Blueprint, jsonify, request
 from sqlalchemy import func
+from inflection import singularize
 from app.models.secex import Secex
 from app import db
 
 blueprint = Blueprint('secex_api', __name__, url_prefix='/secex')
-valid_dimensions = set(['state', 'product', 'trade_partner'])
 
-@blueprint.route('/states/')
-def states():
-    if not set(request.args.keys()).issubset(valid_dimensions):
-        return 'erro', 403
+@blueprint.route('/<path:path>/')
+def states(path):
+    dimensions = map(singularize, path.split('/'))
 
-    dimensions = [Secex.state]
+    if invalid_dimension(request.args.keys()) or invalid_dimension(dimensions):
+        return 'Error', 403
 
-    columns = dimensions + [func.sum(Secex.value), func.sum(Secex.kg)]
-    query = Secex.query.with_entities(*columns).filter_by(**request.args.to_dict()).group_by(*dimensions)
+    filters = request.args.to_dict()
+    group_columns = [getattr(Secex, dimension) for dimension in dimensions]
 
-    return jsonify(data=query.all(), headers=['state', 'value', 'kg'])
+    columns = group_columns + [func.sum(Secex.value), func.sum(Secex.kg)]
+    query = Secex.query.with_entities(*columns).filter_by(**filters).group_by(*group_columns)
+
+    return jsonify(data=query.all(), headers=dimensions + ['value', 'kg'])
+
+
+def invalid_dimension(dimensions):
+    return not set(dimensions).issubset(set(['state', 'product', 'country']))
