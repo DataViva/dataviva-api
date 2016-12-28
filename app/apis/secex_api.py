79b150a8d4ab86ba803f8f5bf97f12d6861be15f
Diogo Lundberg <diogo@lundberg.com.br>
2016-12-28 14:15:20 -0200

    Updates secex api to the new data

--- app/apis/secex_api.py
+++ app/apis/secex_api.py
@@ -1,26 +1,38 @@
-from flask import Blueprint, jsonify, request
-from sqlalchemy import func
+from flask import Blueprint, jsonify, request, render_template
+from sqlalchemy import func, distinct
 from inflection import singularize
-from app.models.secex import Secex
+from app.models.secex import Secex as Model
 from app import db
 
 blueprint = Blueprint('secex_api', __name__, url_prefix='/secex')
 
+@blueprint.route('/sankey/')
+def sankey():
+    return render_template('sankey.html')
+
 @blueprint.route('/<path:path>/')
 def api(path):
     dimensions = map(singularize, path.split('/'))
-
-    if invalid_dimension(request.args.keys()) or invalid_dimension(dimensions):
+    if invalid_dimension(dimensions):
         return 'Error', 403
+        
+    filters = {k: v for k, v in request.args.to_dict().iteritems() if k in Model.dimensions()}
+    counts = [c for c in map(singularize, request.args.getlist('count')) if c in Model.dimensions()]
 
-    filters = request.args.to_dict()
-    group_columns = [getattr(Secex, dimension) for dimension in dimensions]
+    count_columns = get_columns(counts)
+    group_columns = get_columns(dimensions)
+    headers = get_headers(group_columns) + get_headers(count_columns, '_count') + Model.value_headers()
 
-    columns = group_columns + [func.sum(Secex.value), func.sum(Secex.kg)]
-    query = Secex.query.with_entities(*columns).filter_by(**filters).group_by(*group_columns)
+    entities = group_columns + map(lambda x: func.count(distinct(x)), count_columns) + Model.agg_values()
+    query = Model.query.with_entities(*entities).filter_by(**filters).group_by(*group_columns)
 
-    return jsonify(data=query.all(), headers=dimensions + ['value', 'kg'])
+    return jsonify(data=query.all(), headers=headers)
 
+def get_headers(columns, suffix=''):
+    return map(lambda x: x.key + suffix, columns)
+
+def get_columns(dimensions):
+    return [getattr(Model, dimension) for dimension in dimensions]
 
 def invalid_dimension(dimensions):
-    return not set(dimensions).issubset(set(['state', 'product', 'country']))
+    return not set(dimensions).issubset(set(Model.dimensions()))
