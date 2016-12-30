4a370c65ee032abd68e4cb1a19c356930859ba61
Diogo Lundberg <diogo@lundberg.com.br>
2016-12-30 11:37:47 -0200

    Updates api config for deployement

--- config.py
+++ config.py
@@ -5,6 +5,11 @@ class Config(object):
     DEBUG = False
     TESTING = False
     SQLALCHEMY_TRACK_MODIFICATIONS = False
+    REDIS_HOST = 'localhost'
+    REDIS_PORT = '6379'
+    REDIS_DB = '0'
+    S3_ACCESS_KEY = getenv("S3_ACCESS_KEY")
+    S3_SECRET_KEY = getenv("S3_SECRET_KEY")
 
 
 class Production(Config):
@@ -13,18 +18,15 @@ class Production(Config):
         getenv("DATAVIVA_REDSHIFT_PW"),
         getenv("DATAVIVA_REDSHIFT_HOST"),
         getenv("DATAVIVA_REDSHIFT_NAME"))
+    REDIS_HOST = getenv("REDIS_HOST")
+    REDIS_PORT = getenv("REDIS_PORT")
+    REDIS_DB = getenv("REDIS_DB")
 
 
 class Development(Config):
     DEBUG = True
     SQLALCHEMY_DATABASE_URI = "postgresql:///dataviva"
 
-    S3_ACCESS_KEY = getenv("S3_ACCESS_KEY")
-    S3_SECRET_KEY = getenv("S3_SECRET_KEY")
-
-    REDIS_HOST = getenv("REDIS_HOST")
-    REDIS_PORT = getenv("REDIS_PORT")
-    REDIS_DB = getenv("REDIS_DB")
     
 
 class Testing(Config):
