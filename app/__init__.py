3bcad6526d00d2b62cac1c17964c3e4b21138da9
Saulo Antunes <sauloantune@gmail.com>
2017-01-03 20:46:35 +0000

    Add CORS support

--- app/__init__.py
+++ app/__init__.py
@@ -3,6 +3,7 @@ from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_redis import Redis
 from flask_runner import Manager
+from flask_cors import CORS, cross_origin
 from app.helpers.blueprints_helper import register_blueprints
 from app.helpers.json_encoder_helper import ApiJSONEncoder
 
@@ -13,5 +14,6 @@ flask.json_encoder = ApiJSONEncoder
 db = SQLAlchemy(flask)
 redis = Redis(flask)
 manager = Manager(flask)
+cors = CORS(flask, resources={"*": {"origins": "*"}})
 
 register_blueprints(flask, 'apis')
