0cee6364f10c3a131b453ecbc3b1a422c1a22ecb
Saulo Antunes <sauloantune@gmail.com>
2017-01-25 16:02:40 +0000

    Change column names

--- app/models/secex.py
+++ app/models/secex.py
@@ -2,22 +2,22 @@ from sqlalchemy import Column, Integer, String, BigInteger, func
 from app import db
 
 class Secex(db.Model):
-    __tablename__ = 'secex'
-    year          = Column(Integer, primary_key=True)
-    month         = Column(Integer, primary_key=True)
-    type          = Column(String(6), primary_key=True)
-    continent     = Column(String(2), primary_key=True)
-    country       = Column(String(3), primary_key=True)
-    state         = Column(String(2), primary_key=True)
-    mesoregion    = Column(String(4), primary_key=True)
-    microregion   = Column(String(5), primary_key=True)
-    municipality  = Column(String(7), primary_key=True)
-    port          = Column(String(4), primary_key=True)
-    section       = Column(String(2), primary_key=True)
-    chapter       = Column(String(2), primary_key=True)
-    product       = Column(String(4), primary_key=True)
-    value         = Column(BigInteger)
-    kg            = Column(BigInteger)
+    __tablename__   = 'secex'
+    year            = Column(Integer, primary_key=True)
+    month           = Column(Integer, primary_key=True)
+    type            = Column(String(6), primary_key=True)
+    continent       = Column(String(2), primary_key=True)
+    country         = Column(String(3), primary_key=True)
+    state           = Column(String(2), primary_key=True)
+    mesoregion      = Column(String(4), primary_key=True)
+    microregion     = Column(String(5), primary_key=True)
+    municipality    = Column(String(7), primary_key=True)
+    port            = Column(String(4), primary_key=True)
+    product_section = Column(String(2), primary_key=True)
+    product_chapter = Column(String(2), primary_key=True)
+    product         = Column(String(4), primary_key=True)
+    value           = Column(BigInteger)
+    kg              = Column(BigInteger)
 
     @classmethod
     def dimensions(cls):
@@ -32,8 +32,8 @@ class Secex(db.Model):
             'microregion',
             'municipality',
             'port',
-            'section',
-            'chapter',
+            'product_section',
+            'product_chapter',
             'product',
         ]
 
