2abb40fef81befad56991e67647e40cf0777f344
Diogo Lundberg <diogo@lundberg.com.br>
2017-01-28 16:47:37 -0200

    Updates Rais new columns

--- app/models/rais.py
+++ app/models/rais.py
@@ -2,51 +2,63 @@ from sqlalchemy import Column, Integer, String, Numeric, func
 from app import db
 
 class Rais(db.Model):
-    __tablename__        = 'rais'
-    occupation           = Column(String(6), primary_key=True)
-    cnae                 = Column(String(5), primary_key=True)
-    cnae_section         = Column(String(1), primary_key=True)
-    cnae_division        = Column(String(2), primary_key=True)
-    literacy             = Column(String(1), primary_key=True)
-    establishment        = Column(String(14), primary_key=True)
-    simples               = Column(String(1), primary_key=True)
-    municipality         = Column(String(7), primary_key=True)
-    employee             = Column(String(11), primary_key=True)
-    ethnicity            = Column(String(2), primary_key=True)
-    gender               = Column(String(1), primary_key=True)
-    establishment_size   = Column(String(1), primary_key=True)
-    year                 = Column(Integer, primary_key=True)
-    age                  = Column(Integer)
-    wage_received        = Column(Numeric(17,2))
-    average_monthly_wage = Column(Numeric(17,2))
+    __tablename__      = 'rais'
+    region             = Column(String(1), primary_key=True)
+    mesoregion         = Column(String(4), primary_key=True)
+    microregion        = Column(String(5), primary_key=True)
+    state              = Column(String(2), primary_key=True)
+    municipality       = Column(String(7), primary_key=True)
+    occupation_family  = Column(String(4), primary_key=True)
+    occupation_group   = Column(String(1), primary_key=True)
+    industry_class     = Column(String(5), primary_key=True)
+    industry_division  = Column(String(2), primary_key=True)
+    industry_section   = Column(String(1), primary_key=True)
+    establishment      = Column(String(14), primary_key=True)
+    employee           = Column(String(11), primary_key=True)
+    ethnicity          = Column(String(2), primary_key=True)
+    establishment_size = Column(String(2), primary_key=True)
+    gender             = Column(String(1), primary_key=True)
+    legal_nature       = Column(String(2), primary_key=True)
+    literacy           = Column(String(2), primary_key=True)
+    simple             = Column(String(2), primary_key=True)
+    year               = Column(Integer, primary_key=True)
+    age                = Column(Integer)
+    wage               = Column(Numeric(17,2))
+
 
     @classmethod
     def dimensions(cls):
         return [
-            'occupation',
-            'cnae',
-            'cnae_section',
-            'cnae_division',
-            'literacy',
-            'establishment',
-            'simples',
+            'region',
+            'mesoregion',
+            'microregion',
+            'state',
             'municipality',
+            'occupation_family',
+            'occupation_group',
+            'industry_class',
+            'industry_division',
+            'industry_section',
+            'establishment',
             'employee',
             'ethnicity',
-            'gender',
             'establishment_size',
+            'gender',
+            'legal_nature',
+            'literacy',
+            'simple',
             'year',
         ]
 
     @classmethod
-    def agg_values(cls):
-        return [
-            func.avg(cls.age),
-            func.avg(cls.average_monthly_wage),
-            func.sum(cls.wage_received),
-            func.count(cls.employee),
-        ]
+    def aggregate(cls, value):
+        return {
+            'average_age': func.avg(cls.age),
+            'average_wage': func.avg(cls.wage),
+            'wage': func.sum(cls.wage),
+            'jobs': func.count(cls.employee),
+        }[value]
 
     @classmethod
-    def value_headers(cls):
-        return ['age', 'average_monthly_wage', 'wage_received', 'jobs']
+    def values(cls):
+        return ['average_age', 'average_wage', 'wage', 'jobs']
