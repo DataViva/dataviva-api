306d8752f2f8f5d07569a29a27aa83046628ed6b
Saulo Antunes <sauloantuness@gmail.com>
2017-02-14 22:26:53 +0000

    Add sort, direction and limit functions and create dataset_api

--- app/apis/datasets_api.py
+++ app/apis/datasets_api.py
@@ -1,21 +1,25 @@
 from flask import Blueprint, jsonify, request
 from sqlalchemy import func, distinct
+from importlib import import_module
 from inflection import singularize
-from app.models.hedu import Hedu as Model
 from app import cache
 from app.helpers.cache_helper import api_cache_key
 
-blueprint = Blueprint('hedu_api', __name__, url_prefix='/hedu')
+blueprint = Blueprint('api', __name__, url_prefix='/')
 
-@blueprint.route('/<path:path>/')
-@cache.cached(key_prefix=api_cache_key("hedu"))
-def api(path):
+@blueprint.route('<dataset>/<path:path>/')
+@cache.cached(key_prefix=api_cache_key('dataset'))
+def api(dataset, path):
+    global Model
+    Model = getattr(import_module('app.models.' + dataset), dataset.title())
+    
     dimensions = map(singularize, path.split('/'))
     if invalid_dimension(dimensions):
         return 'Error', 403
 
     filters = {k: v for k, v in request.args.to_dict().iteritems() if k in Model.dimensions()}
     counts = [c for c in map(singularize, request.args.getlist('count')) if c in Model.dimensions()]
+
     values = get_values(request)
 
     group_columns = get_columns(dimensions)
@@ -25,6 +29,18 @@ def api(path):
     headers = get_headers(group_columns) + get_headers(count_columns, '_count') + values
     entities = group_columns + map(lambda x: func.count(distinct(x)), count_columns) + aggregated_values
     query = Model.query.with_entities(*entities).filter_by(**filters).group_by(*group_columns)
+
+    order = request.args.get('order', None)
+    direction = request.args.get('direction', '')
+    if order:
+        if direction.lower() == 'desc':
+            query = query.order_by(getattr(Model, order).desc())
+        else:
+            query = query.order_by(getattr(Model, order))
+
+    limit = request.args.get('limit', None)
+    if limit:
+        query = query.limit(limit)
 
     return jsonify(data=query.all(), headers=headers)
 
