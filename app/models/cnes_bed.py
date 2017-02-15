794473af5b162f308d35d6f92f5f7bd79380ff49
Joao Victor Miranda <jvdmmiranda@gmail.com>
2017-02-15 18:07:51 -0200

    Create Cnes equipment and Cnes professional models

---
+++ app/models/cnes_bed.py
@@ -0,0 +1,33 @@
+from sqlalchemy import Column, Integer, String, func
+from app import db
+
+class CnesBed(db.Model):
+    __tablename__ = 'cnes_bed'
+    year          = Column(Integer, primary_key=True)
+    region        = Column(String(1), primary_key=True)
+    mesoregion    = Column(String(4), primary_key=True)
+    microregion   = Column(String(5), primary_key=True)
+    state         = Column(String(2), primary_key=True)
+    municipality  = Column(String(7), primary_key=True)
+    cnes          = Column(String(7), primary_key=True)
+
+    @classmethod    
+    def dimensions(cls):
+        return [
+            'year',
+            'region',
+            'mesoregion',
+            'microregion',
+            'state',
+            'municipality',
+        ]
+
+    @classmethod
+    def aggregate(cls, value):
+        return {
+            'beds': func.count(cls.cnes)
+        }[value]
+
+    @classmethod
+    def values(cls):
+        return ['beds']
