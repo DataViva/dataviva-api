d926f321a26fe7c6b72513f88fe60bc4f3c899e4
daniel1409 <danieltafuri1998@gmail.com>
2017-03-27 14:34:59 -0300

    Update model cnes bed

--- app/models/cnes_bed.py
+++ app/models/cnes_bed.py
@@ -2,15 +2,22 @@ from sqlalchemy import Column, Integer, String, func
 from app import db
 
 class CnesBed(db.Model):
-    __tablename__ = 'cnes_bed'
-    year          = Column(Integer, primary_key=True)
-    region        = Column(String(1), primary_key=True)
-    mesoregion    = Column(String(4), primary_key=True)
-    microregion   = Column(String(5), primary_key=True)
-    state         = Column(String(2), primary_key=True)
-    municipality  = Column(String(7), primary_key=True)
-    cnes          = Column(String(7), primary_key=True)
-    bed_type      = Column(String(7), primary_key=True)
+    __tablename__                               = 'cnes_bed'
+    year                                        = Column(Integer, primary_key=True)
+    region                                      = Column(String(1), primary_key=True)
+    mesoregion                                  = Column(String(4), primary_key=True)
+    microregion                                 = Column(String(5), primary_key=True)
+    state                                       = Column(String(2), primary_key=True)
+    municipality                                = Column(String(7), primary_key=True)
+    establishment                               = Column(String(7), primary_key=True)
+    unit_type                                   = Column(String(2), primary_key=True)
+    bed_type                                    = Column(String(1), primary_key=True)
+    bed_type_per_specialty                      = Column(String(2), primary_key=True)
+    number_existing_bed                         = Column(Integer, primary_key=True)
+    number_existing_contract                    = Column(Integer, primary_key=True)
+    number_sus_bed                              = Column(Integer, primary_key=True)
+    number_non_sus_bed                          = Column(Integer, primary_key=True)
+    health_region                               = Column(String(5), primary_key=True)
 
     @classmethod    
     def dimensions(cls):
@@ -21,8 +28,10 @@ class CnesBed(db.Model):
             'microregion',
             'state',
             'municipality',
-            'cnes',
+            'establishment',
             'bed_type',
+            'bed_type_per_specialty',
+            'health_region'
         ]
 
     @classmethod
