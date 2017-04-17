9d4bae1183285361536e200c953cbe12c990b7f7
Saulo Antunes <sauloantuness@gmail.com>
2017-04-17 14:49:13 -0300

    Add hedu course field to hedu course metadata

--- app/scripts/load_metadata_command.py
+++ app/scripts/load_metadata_command.py
@@ -542,20 +542,23 @@ def load_hedu_course():
     hedu_courses_field = {}
 
     for _, row in df.iterrows():
-
         if len(row['id']) == 2:
             hedu_course_field = {
+                'id': row['id'],
                 'name_pt': row["name_pt"],
-                'name_en': row["name_en"]
+                'name_en': row["name_en"],
             }
 
             redis.set('hedu_course_field/' + str(row['id']), pickle.dumps(hedu_course_field))
             hedu_courses_field[row['id']] = hedu_course_field
 
-        elif len(row['id']) == 6:
+    for _, row in df.iterrows():
+        if len(row['id']) == 6:
             hedu_course = {
+                'id': row['id'],
                 'name_pt': row["name_pt"],
-                'name_en': row["name_en"]
+                'name_en': row["name_en"],
+                'hedu_course_field': hedu_courses_field[row['id'][:2]]
             }
 
             redis.set('hedu_course/' + str(row['id']), pickle.dumps(hedu_course))
