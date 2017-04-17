36e3971560165b531c4ddb3b0daa06ebf0968e30
luisccf <luisccf@me.com>
2017-04-17 15:10:39 -0300

    Adds inflection metadata

--- app/scripts/load_metadata_command.py
+++ app/scripts/load_metadata_command.py
@@ -569,6 +569,32 @@ def load_hedu_course():
 
     print "HEDU Courses loaded."
 
+def load_inflections():
+    csv = read_csv_from_s3('redshift/attrs/attrs_infleccoes.csv')
+    df = pd.read_csv(
+            csv,
+            sep=';',
+            header=0,
+            names=['id','name_en','name_pt','gender','plural']
+        )
+
+    inflections = {}
+
+    for _, row in df.iterrows():
+        inflection = {
+            'id': row['id'],
+            'name_en': row['name_en'],
+            'name_pt': row['name_pt'],
+            'gender': row['gender'],
+            'plural': row['plural']
+        }
+        inflections[row['id']] = inflection
+        redis.set('inflection/' + str(row['id']), pickle.dumps(inflection))
+
+    redis.set('inflection', pickle.dumps(inflections))
+
+    print "Inflections loaded."
+
 def load_attrs(attrs):
     for attr in attrs:
         print 'Loading %s ...' % attr['name'],
@@ -606,6 +632,7 @@ class LoadMetadataCommand(Command):
     """
 
     def run(self):
+        load_inflections()
         load_continent()
         load_countries()
         load_regions()
