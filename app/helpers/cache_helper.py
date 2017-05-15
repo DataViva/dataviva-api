780a1112fe8dbd7866fbf747b4558ce7c484632a
Frederico Vieira <fredops1@hotmail.com>
2017-05-15 16:35:18 -0300

    Fixes language layout patterns.

--- app/helpers/cache_helper.py
+++ app/helpers/cache_helper.py
@@ -1,18 +1,19 @@
 from flask import get_flashed_messages, request
-                    
+
+
 def api_cache_key(namespace, *args, **kwargs):
     def gen_key(**kwargs):
         path = request.path
         reqstr = ""
         if request.args:
-            for k,v in request.args.items():
+            for k, v in request.args.items():
                 reqstr += "&{}={}".format(str(k), str(v))
         key = namespace + ":" + path + reqstr
         cache_key = key.encode('utf-8')
 
         if get_flashed_messages():
             msgs = "|".join([msg[0] for msg in get_flashed_messages(with_categories=True)])
-            cache_key += "/"+msgs
+            cache_key += "/" + msgs
 
         return cache_key
     return gen_key
