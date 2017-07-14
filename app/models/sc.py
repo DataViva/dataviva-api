871c2389750e582444189ddcafc9732f22da160c
Saulo Antunes <sauloantuness@gmail.com>
2017-07-14 16:40:37 -0300

    add columns school and administrative dependency

--- app/models/sc.py
+++ app/models/sc.py
@@ -3,19 +3,21 @@ from app import db
 
 class Sc(db.Model):
     __tablename__    = 'sc'
-    year             = Column(Integer, primary_key=True)
-    region           = Column(String(1), primary_key=True)
-    mesoregion       = Column(String(4), primary_key=True)
-    microregion      = Column(String(5), primary_key=True)
-    state            = Column(String(2), primary_key=True)
-    municipality     = Column(String(7), primary_key=True)
-    university       = Column(String(5), primary_key=True)
-    sc_course_field  = Column(String(2), primary_key=True)
-    sc_course        = Column(String(6), primary_key=True)
-    sc_class         = Column(String(2), primary_key=True)
-    gender           = Column(String(2), primary_key=True)
-    ethnicity        = Column(String(2), primary_key=True)
-    age              = Column(Integer)
+    year                      = Column(Integer, primary_key=True)
+    region                    = Column(String(1), primary_key=True)
+    mesoregion                = Column(String(4), primary_key=True)
+    microregion               = Column(String(5), primary_key=True)
+    state                     = Column(String(2), primary_key=True)
+    municipality              = Column(String(7), primary_key=True)
+    university                = Column(String(5), primary_key=True)
+    sc_course_field           = Column(String(2), primary_key=True)
+    sc_course                 = Column(String(5), primary_key=True)
+    sc_school                 = Column(String(8), primary_key=True)
+    sc_class                  = Column(String(8), primary_key=True)
+    administrative_dependency = Column(String(1), primary_key=True)
+    age                       = Column(Integer)
+    gender                    = Column(String(1), primary_key=True)
+    ethnicity                 = Column(String(2), primary_key=True)
 
     @classmethod
     def dimensions(cls):
@@ -29,7 +31,9 @@ class Sc(db.Model):
             'university',
             'sc_course_field',
             'sc_course',
+            'sc_school',
             'sc_class',
+            'administrative_dependency',
             'gender',
             'ethnicity',
         ]
