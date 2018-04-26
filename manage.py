85eb10ea7c3982d36bdbaac3be44f4f286ec4eef
rafaelsmedina <r.salesmf@gmail.com>
2018-04-26 11:16:26 -0300

    Rename loadmetadata command

--- manage.py
+++ manage.py
@@ -1,6 +1,6 @@
 from app import manager
 from app.scripts import LoadMetadataCommand, LoadScCourse
 
-manager.add_command('loadmetadata', LoadMetadataCommand)
+manager.add_command('load_metadata', LoadMetadataCommand)
 manager.add_command('load_sc_course', LoadScCourse)
 manager.run()
