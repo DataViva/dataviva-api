2a052fd0871949e5c7d2c7bcf5b9a06cdb774653
Italo Daldegan <italodaldegan@hotmail.com>
2018-05-30 11:26:23 -0300

    Fix redis config

--- config.py
+++ config.py
@@ -7,16 +7,16 @@ class Config(object):
     HIDE_DATA = True
     SQLALCHEMY_TRACK_MODIFICATIONS = False
 
-    CACHE_TYPE: getenv('CACHE_TYPE', 'redis')
-    CACHE_KEY_PREFIX: getenv('CACHE_KEY_PREFIX', 'api')
-    CACHE_DEFAULT_TIMEOUT: getenv('CACHE_DEFAULT_TIMEOUT', 60000000)
-    CACHE_REDIS_HOST: getenv('CACHE_REDIS_HOST', 'localhost')
-    CACHE_REDIS_PORT: getenv('CACHE_REDIS_PORT', 6379)
-    CACHE_REDIS_DB: getenv('CACHE_REDIS_DB', 0)
+    CACHE_TYPE = getenv('CACHE_TYPE', 'redis')
+    CACHE_KEY_PREFIX = getenv('CACHE_KEY_PREFIX', 'api')
+    CACHE_DEFAULT_TIMEOUT = getenv('CACHE_DEFAULT_TIMEOUT', 60000000)
+    CACHE_REDIS_HOST = getenv('CACHE_REDIS_HOST', 'localhost')
+    CACHE_REDIS_PORT = getenv('CACHE_REDIS_PORT', 6379)
+    CACHE_REDIS_DB = getenv('CACHE_REDIS_DB', 0)
 
     REDIS_HOST = getenv('REDIS_HOST', 'localhost')
-    REDIS_PORT = getenv('REDIS_PORT', '6379')
-    REDIS_DB = getenv('REDIS_DB', '0')
+    REDIS_PORT = getenv('REDIS_PORT', 6379)
+    REDIS_DB = getenv('REDIS_DB', 0)
 
     S3_PUBLIC_BUCKET_URL = getenv("S3_PUBLIC_BUCKET_URL")
 
