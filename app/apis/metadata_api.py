0ff5fd8e2ec4f8d9355f88b5a884ce5485ea230c
rafaelsmedina <r.salesmf@gmail.com>
2018-07-23 16:50:28 -0300

    Fix incorrect naming

--- app/apis/metadata_api.py
+++ app/apis/metadata_api.py
@@ -7,14 +7,14 @@ blueprint = Blueprint('metadata_api', __name__, url_prefix='/metadata')
 
 
 @blueprint.route('/<string:data>/<string:id_item>')
-def item(data, id_item):
+def get_item(data, id_item):
     item = "{}/{}".format(singularize(data), id_item)
 
     return jsonify(json.loads(redis.get(item)))
 
 
 @blueprint.route('/<string:data>')
-def all(data):
+def get_all(data):
     file_name = "{}.json".format(singularize(data))
 
     return redirect(flask.config['S3_PUBLIC_BUCKET_URL'] + file_name)
